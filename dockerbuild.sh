490e6335fc283333eb29349501328fac03419178
Ari Freyr <aria12@ru.is>
2014-11-28 15:08:34 +0000

    changed build file

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 export PATH=$PATH:/usr/local/bin
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/DrepAri/.boot2docker/certs/boot2docker-vm
 
 echo Cleaning...
 rm -rf ./dist
 
 echo Building app
-rm -rf node_modules && npm cache clean && npm i
+npm install
+bower install
 grunt
 
 cp ./Dockerfile ./dist/
