f28d0722fe1c5012bc6dbd06dff5eafba603d055
Ari Freyr <aria12@ru.is>
2014-12-01 17:31:20 +0000

    stuff

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -10,8 +10,8 @@ echo "Cleaning..."
 rm -rf ./dist
 
 echo "Building app"
-#rm -rf node_modules && npm cache clean && npm i
-npm install
+rm -rf node_modules && npm cache clean && npm i
+#npm install
 bower install
 
 grunt
