2e392adab63d52a469cfccaf072016ddc7ffe2de
Ari Freyr <aria12@ru.is>
2014-12-02 11:05:20 +0000

    stuff

--- getdockerimage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker kill $(docker ps -a -q)
-docker pull arifreyr/tictactoe
-docker run -p 80:8080 -d -e "NODE_ENV=production" arifreyr/tictactoe
--- push2production.sh
+++ push2production.sh
@@ -2,5 +2,12 @@
 
 set -e
 echo "Sending commands....."
-ssh root@178.62.218.30 'bash -s' < getdockerimage.sh
+ssh -o StrictHostKeyChecking=no root@178.62.218.30 "
+docker kill $(docker ps -a -q)
+
+docker pull arifreyr/tictactoe
+
+docker run -p 80:8080 -d -e 'NODE_ENV=production' arifreyr/tictactoe
+"
+
 echo "Done..."
