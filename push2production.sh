aeb93b562e40515e2b70b1c4b680f58a551c6908
Ari Freyr <aria12@ru.is>
2014-12-02 11:40:50 +0000

    production script should be working

--- push2production.sh
+++ push2production.sh
@@ -3,11 +3,13 @@
 set -e
 echo "Sending commands....."
 ssh -o StrictHostKeyChecking=no root@178.62.218.30 "
-docker kill $(docker ps -a -q)
+docker kill tictactoe
+
+docker rm tictactoe
 
 docker pull arifreyr/tictactoe
 
-docker run -p 80:8080 -d -e 'NODE_ENV=production' arifreyr/tictactoe
+docker run -p 80:8080 -d -e 'NODE_ENV=production' --name='tictactoe' arifreyr/tictactoe
 "
 
 echo "Done..."
